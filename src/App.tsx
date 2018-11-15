68a3d4541172df42d555ba41b09f763cbc008a34
adamgoodapp <adamgoodapp@gmail.com>
2018-11-16 01:35:15 +0900

    Add simple grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import * as React from "react";
+import SoundGrid from "./components/SoundGrid/SoundGrid";
 import "./App.css";
 
 class App extends React.Component {
+  public componentDidMount() {
+    document.title = "Your chromesthesia sound experience";
+  }
+
   public render() {
     return (
       <div className="App">
-        <p />
+        <SoundGrid />
       </div>
     );
   }
---
+++ src/components/SoundGrid/SoundGrid.tsx
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import "./SoundGrid.css";
+
+class SoundGrid extends Component {
+  render() {
+    return (
+      <div className="grid-container">
+        <div className="item" />
+        <div className="item" />
+        <div className="item" />
+        <div className="item" />
+        <div className="item" />
+        <div className="item" />
+        <div className="item" />
+        <div className="item" />
+        <div className="item" />
+        <div className="item" />
+        <div className="item" />
+        <div className="item" />
+      </div>
+    );
+  }
+}
+
+export default SoundGrid;

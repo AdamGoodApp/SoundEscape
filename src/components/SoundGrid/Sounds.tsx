75101c404518a5451b83d91dc19a6c184db51bef
adamgoodapp <adamgoodapp@gmail.com>
2019-01-07 22:04:28 +0900

    Change sounds

--- src/components/SoundGrid/Sounds.tsx
+++ src/components/SoundGrid/Sounds.tsx
@@ -1,50 +1,14 @@
 export const Sounds = [
-  {
-    color: "#1abc9c",
-    url: "1GptvJSTUY1fopRi3MTGKUYmZUhF9yTqk"
-  },
-  {
-    color: "#2ecc71",
-    url: "1qwLYJl4RvSdz7zUX6AxkSAZIMCqTi4bc"
-  },
-  {
-    color: "#3498db",
-    url: "14iHNZToC0dVUAZYiM3l9wBtuE4E_Oz8x"
-  },
-  {
-    color: "#9b59b6",
-    url: "1ZL9IloYWlSvRsNnNctlF52X-PjWJIlSw"
-  },
-  {
-    color: "#34495e",
-    url: "11TPaa-ZGwAgikr4k4oxF2JYD2OTuhY8C"
-  },
-  {
-    color: "#f1c40f",
-    url: "1Hwx0_Fq4rFbYWdlSCjinl7VfOcaFhjF8"
-  },
-  {
-    color: "#e67e22",
-    url: "1ey5Ds50XjtAq64I3EKTP3X5n8jhkEwXA"
-  },
-  {
-    color: "#e74c3c",
-    url: "1xWsv2CDohOVzqbZPjCL9jItyXCoMHCDu"
-  },
-  {
-    color: "#7f8c8d",
-    url: "1kqUnmGFiLqUZdkpwS3g_qu7Uy8MrOXcM"
-  },
-  {
-    color: "#95a5a6",
-    url: "1sOr-I2bDaICeaLuiv57aIt7QnVkDmRTh"
-  },
-  {
-    color: "#f39c12",
-    url: "1fBrJdOSTybORrlZmpn87tKO8gV9W1wdJ"
-  },
-  {
-    color: "#d35400",
-    url: "1wQs70WKMqdTG84XtZ1q-rPSV1hDL4nkz"
-  }
+  { color: "#1abc9c", url: "1GptvJSTUY1fopRi3MTGKUYmZUhF9yTqk" },
+  { color: "#2ecc71", url: "1qwLYJl4RvSdz7zUX6AxkSAZIMCqTi4bc" },
+  { color: "#3498db", url: "14iHNZToC0dVUAZYiM3l9wBtuE4E_Oz8x" },
+  { color: "#9b59b6", url: "1ZL9IloYWlSvRsNnNctlF52X-PjWJIlSw" },
+  { color: "#34495e", url: "11TPaa-ZGwAgikr4k4oxF2JYD2OTuhY8C" },
+  { color: "#f1c40f", url: "1Hwx0_Fq4rFbYWdlSCjinl7VfOcaFhjF8" },
+  { color: "#e67e22", url: "1ey5Ds50XjtAq64I3EKTP3X5n8jhkEwXA" },
+  { color: "#e74c3c", url: "1LimtUVHD57PXRxEsDHjX9jcOZa0iT40x" },
+  { color: "#7f8c8d", url: "1kqUnmGFiLqUZdkpwS3g_qu7Uy8MrOXcM" },
+  { color: "#95a5a6", url: "1sOr-I2bDaICeaLuiv57aIt7QnVkDmRTh" },
+  { color: "#f39c12", url: "1fBrJdOSTybORrlZmpn87tKO8gV9W1wdJ" },
+  { color: "#d35400", url: "1I6Y5Oo7lwChZdVK55Dhh1ebzUJNRTXJH" }
 ];
